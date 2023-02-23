6287c9bce6f7722b4d990725ff3d8ccc42d60813
Anusha <anushamns@gmail.com>
2023-02-23 13:03:56 +0530

    added the missing username field

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 # Create your views here.
 def home(request):
     return render(request, "authentication/home.html")
 
 def signup(request):
+    #
+    # if request.method == 'POST':
+    #     Username = request.POST['Username']
+    #     FirstName = request.POST['Firstname']
+    #     LastName = request.POST['Lastname']
+    #     Email = request.POST['Email']
+    #     Phone = request.POST['Phone']
+    #     Password = request.POST['Password']
+    #     CPassword = request.POST['CPassword']
+    #     myuser = User.objects.create_user()
+    #
+
     return render(request, "authentication/signup.html")
 
 def signin(request):
