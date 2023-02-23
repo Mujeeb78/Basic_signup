9d216b68b267acc3dddc70b771698bfbf42df6db
Anusha <anushamns@gmail.com>
2023-02-23 11:41:20 +0530

    first commit

---
+++ authentication/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+# Create your views here.
+def home(request):
+    return render(request, "authentication/home.html")
+
+def signup(request):
+    return render(request, "authentication/signup.html")
+
+def signin(request):
+    return render(request, "authentication/signin.html")
+
+def signout(request):
+    pass
